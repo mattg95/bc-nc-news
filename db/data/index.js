2de8130274085ec60c4f159280563f91f529c5b8
mattg95 <mattgoodman95@gmail.com>
2019-08-23 15:30:39 +0100

    added correct routing for index file

---
+++ db/data/index.js
@@ -0,0 +1,7 @@
+const ENV = process.env.NODE_ENV || "development";
+const development = require("./development-data");
+const test = require("./test-data");
+
+const data = { development, test };
+
+module.exports = data[ENV];
