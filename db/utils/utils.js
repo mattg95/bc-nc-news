efec2aaeb8c993b0467f92365114b4ade8538368
mattg95 <mattgoodman95@gmail.com>
2019-08-27 17:44:53 +0100

    finally made two working utils - formatDates and makeRefObj

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,5 +1,27 @@
-exports.formatDates = list => {};
+exports.formatDates = list => {
+  return list.map((listObj)=>{
+  const timestamp = listObj.created_at;
+  if (timestamp === undefined){ return listObj}
+  const newDate = new Date(timestamp)
+  const date = `${newDate.getDate()}/${newDate.getMonth()}/${newDate.getFullYear()}`
+  listObj.created_at = date;
+  return listObj
+  }
+  )
 
-exports.makeRefObj = list => {};
+};
+
+exports.makeRefObj = list => {
+  let obj = {};
+  for (let i = 0; i < list.length; i++) {
+    const id = list[i].article_id;
+    const title = list[i].title;
+    if (title !== undefined) {
+      obj[title] = id;
+      console.log(obj);
+    }
+  }
+  return obj;
+};
 
 exports.formatComments = (comments, articleRef) => {};
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -1,12 +1,81 @@
-const { expect } = require('chai');
+const { expect } = require("chai");
 const {
   formatDates,
   makeRefObj,
-  formatComments,
-} = require('../db/utils/utils');
+  formatComments
+} = require("../db/utils/utils");
 
-describe('formatDates', () => {});
+describe.only("formatDates", () => {
+  it("when passed an array of one empty object, returns an array of one empty object", () => {
+    const input = [{}]
+    const output = [{}]
+    expect(formatDates(input)).to.deep.equal(output);
+  });
+  it("when passed a date formatted as a UNIX timestamp, returns the same date as a javascript date object", () => {
+    const input = [{ created_at: 0 }]
+    const output = [{ created_at: '1/0/1970' }]
+    expect(formatDates(input)).to.deep.equal(output);
+  });
+  it("when passed a comment, changes only the date property to a javascript date object", () => {
+    const input = [{
+      body:
+        "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+      belongs_to: "They're not exactly dogs, are they?",
+      created_by: 'butter_bridge',
+      votes: 16,
+      created_at: 1511354163389,
+    }]
+    const output = [{ body:
+        "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+      belongs_to: "They're not exactly dogs, are they?",
+      created_by: 'butter_bridge',
+      votes: 16,
+      created_at: '22/10/2017'
+  }]
+    expect(formatDates(input)).to.deep.equal(output);
+  })
+  it("works for an array of multiple objects", () => {
+    const input = [{
+      body:
+        "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+      belongs_to: "They're not exactly dogs, are they?",
+      created_by: 'butter_bridge',
+      votes: 16,
+      created_at: 1511354163389,
+    },
+    { created_at: 0 }]
+    const output = [{ body:
+        "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+      belongs_to: "They're not exactly dogs, are they?",
+      created_by: 'butter_bridge',
+      votes: 16,
+      created_at: '22/10/2017'},
+      { created_at: '1/0/1970' }
+    ]
+    expect(formatDates(input)).to.deep.equal(output)
+  })
+})
 
-describe('makeRefObj', () => {});
 
-describe('formatComments', () => {});
+describe("makeRefObj", () => {
+  it("takes an array of objects as input and returns an object", () => {
+    const input = [{}];
+    const output = {};
+    expect(makeRefObj(input)).to.deep.equal(output);
+  });
+  it("transforms each object so that the article id corresponds with the title", () => {
+    const input = [{ article_id: 1, title: "A" }];
+    const output = { A: 1 };
+    expect(makeRefObj(input)).to.deep.equal(output);
+  });
+  it("works over an array of multiple objects", () => {
+    const input = [
+      { article_id: 1, title: "A" },
+      { article_id: 2, title: "B" }
+    ];
+    const output = { A: 1, B: 2 };
+    expect(makeRefObj(input)).to.deep.equal(output);
+  });
+});
+
+describe.only("formatComments", () => {});
