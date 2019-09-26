ab55cd9be4b4c6fbb372dfa6857cee1dbca0526a
mattg95 <mattgoodman95@gmail.com>
2019-09-26 10:39:42 +0100

    all tests now nested perfectly

--- spec/errorHandling.spec.js
+++ spec/errorHandling.spec.js
@@ -12,28 +12,13 @@ describe("/", () => {
   ///////////////////////
   beforeEach(() => connection.seed.run());
   after(() => connection.destroy());
-  describe("ALL /not_a_route", () => {
-    it("STATUS:404 get request route not found", () => {
-      const methods = ["post", "patch", "delete", "get"];
-      const promises = methods.map(method => {
-        return request(app)
-          [method]("/notAroute")
-          .expect(404)
-          .then(res => {
-            expect(res.body.msg).to.equal("route not found");
-          });
-      });
-      return Promise.all(promises);
-    });
-  });
-  ////////////////////////
-  describe("/api", () => {
-    describe("ALL /api/not_a_route", () => {
+  describe("/not_a_route", () => {
+    describe("ALL", () => {
       it("STATUS:404 get request route not found", () => {
         const methods = ["post", "patch", "delete", "get"];
         const promises = methods.map(method => {
           return request(app)
-            [method]("/api/notAroute")
+            [method]("/notAroute")
             .expect(404)
             .then(res => {
               expect(res.body.msg).to.equal("route not found");
@@ -42,9 +27,28 @@ describe("/", () => {
         return Promise.all(promises);
       });
     });
+  });
+  ////////////////////////
+  describe("/api", () => {
+    describe("/api/not_a_route", () => {
+      describe("ALL", () => {
+        it("STATUS:404 get request route not found", () => {
+          const methods = ["post", "patch", "delete", "get"];
+          const promises = methods.map(method => {
+            return request(app)
+              [method]("/api/notAroute")
+              .expect(404)
+              .then(res => {
+                expect(res.body.msg).to.equal("route not found");
+              });
+          });
+          return Promise.all(promises);
+        });
+      });
+    });
     /////////////////////////////////
     describe("/api/topics", () => {
-      describe("GET /api/topics", () => {
+      describe("GET", () => {
         it(" STATUS:400 bad request", () => {
           return request(app)
             .get("/api/topics/notatopic")
@@ -55,39 +59,43 @@ describe("/", () => {
     });
     //////////////////////////////////
     describe("/api/users", () => {
-      describe("GET /api/users:username", () => {
-        it(" STATUS:404 user not found", () => {
-          return request(app)
-            .get("/api/users/notauser")
-            .expect(404)
-            .then(res => expect(res.body.msg).to.equal("route not found"));
+      describe("/api/users/:username", () => {
+        describe("GET", () => {
+          it(" STATUS:404 user not found", () => {
+            return request(app)
+              .get("/api/users/notauser")
+              .expect(404)
+              .then(res => expect(res.body.msg).to.equal("route not found"));
+          });
         });
       });
     });
     ////////////////////////////////////
     describe("/api/articles", () => {
       ///////////////////////////////////
-      describe("GET /api/articles?query", () => {
-        it("STATUS 400 bad request (sort_by a column that doesn't exist)", () => {
-          return request(app)
-            .get("/api/articles?sort_by=yes")
-            .expect(400)
-            .then(res => {
-              expect(res.body.msg).to.equal("bad request");
-            });
-        });
-        it("STATUS 400 bad request (order !== asc or desc)", () => {
-          return request(app)
-            .get("/api/articles?order=random")
-            .expect(400)
-            .then(res => {
-              expect(res.body.msg).to.equal("bad request");
-            });
+      describe("/api/articles?query", () => {
+        describe("GET", () => {
+          it("STATUS 400 bad request (sort_by a column that doesn't exist)", () => {
+            return request(app)
+              .get("/api/articles?sort_by=yes")
+              .expect(400)
+              .then(res => {
+                expect(res.body.msg).to.equal("bad request");
+              });
+          });
+          it("STATUS 400 bad request (order !== asc or desc)", () => {
+            return request(app)
+              .get("/api/articles?order=random")
+              .expect(400)
+              .then(res => {
+                expect(res.body.msg).to.equal("bad request");
+              });
+          });
         });
       });
       describe("/api/articles/:article_id", () => {
-        describe("GET /api/articles/:wrong_format_id", () => {
-          it("STATUS:400 bad request", () => {
+        describe("GET", () => {
+          it("STATUS:400 bad request (wrong_format_id)", () => {
             return request(app)
               .get("/api/articles/six")
               .expect(400)
@@ -96,8 +104,8 @@ describe("/", () => {
               });
           });
         });
-        describe("PATCH /api/articles:wrong_format_id", () => {
-          it("STATUS:400 bad request", () => {
+        describe("PATCH", () => {
+          it("STATUS:400 bad request (wrong_format_id", () => {
             return request(app)
               .patch("/api/articles/six")
               .send({ inc_votes: 3 })
@@ -107,55 +115,47 @@ describe("/", () => {
               });
           });
         });
-        describe("PATCH /api/articles:article_id (no data sent in patch request)", () => {
-          it("STATUS: 400 bad request", () => {
-            return request(app)
-              .patch("/api/articles/1")
-              .send()
-              .expect(400)
-              .then(res => {
-                expect(res.body.msg).to.equal("bad request");
-              });
-          });
+        it("STATUS: 400 bad request(no data sent in patch request)", () => {
+          return request(app)
+            .patch("/api/articles/1")
+            .send()
+            .expect(400)
+            .then(res => {
+              expect(res.body.msg).to.equal("bad request");
+            });
         });
-        describe("PATCH /api/articles:article_id (no inc_votes key patch request)", () => {
-          it("STATUS: 400 bad request", () => {
-            return request(app)
-              .patch("/api/articles/1")
-              .send({ randomKey: 2 })
-              .expect(400)
-              .then(res => {
-                expect(res.body.msg).to.equal("bad request");
-              });
-          });
+        it("STATUS: 400 bad request (no inc_votes key patch request)", () => {
+          return request(app)
+            .patch("/api/articles/1")
+            .send({ randomKey: 2 })
+            .expect(400)
+            .then(res => {
+              expect(res.body.msg).to.equal("bad request");
+            });
         });
-        describe("PATCH /api/articles:article_id (invalid inc_votes value)", () => {
-          it("STATUS: 400 bad request", () => {
-            return request(app)
-              .patch("/api/articles/1")
-              .send({ inc_votes: "yes" })
-              .expect(400)
-              .then(res => {
-                expect(res.body.msg).to.equal("bad request");
-              });
-          });
+        it("STATUS: 400 bad request (invalid inc_votes value)", () => {
+          return request(app)
+            .patch("/api/articles/1")
+            .send({ inc_votes: "yes" })
+            .expect(400)
+            .then(res => {
+              expect(res.body.msg).to.equal("bad request");
+            });
         });
-        describe("PATCH /api/articles:article_id (extra property on request body)", () => {
-          it("STATUS: 400 bad request", () => {
-            return request(app)
-              .patch("/api/articles/1")
-              .send({ inc_votes: 2, mitch: "present" })
-              .expect(400)
-              .then(res => {
-                expect(res.body.msg).to.equal("bad request");
-              });
-          });
+        it("STATUS: 400 bad request (extra property on request body)", () => {
+          return request(app)
+            .patch("/api/articles/1")
+            .send({ inc_votes: 2, mitch: "present" })
+            .expect(400)
+            .then(res => {
+              expect(res.body.msg).to.equal("bad request");
+            });
         });
       });
       describe("api/articles/:article_id/comments", () => {
         ///////////////////////////////////////
-        describe("POST /api/articles:article_id/comments (bad request)", () => {
-          it("STATUS 400 bad request", () => {
+        describe("POST ", () => {
+          it("STATUS 400 bad request (wrong format article_id)", () => {
             return request(app)
               .post("/api/articles/one/comments")
               .send({
@@ -167,9 +167,8 @@ describe("/", () => {
                 expect(res.body.msg).to.equal("bad request");
               });
           });
-        });
-        describe("POST /api/articles:article_id/comments (invalid article_id)", () => {
-          it("STATUS 404 route not found", () => {
+
+          it("STATUS 404 route not found (invalid article_id)", () => {
             return request(app)
               .post("/api/articles/3000/comments")
               .send({
@@ -181,9 +180,7 @@ describe("/", () => {
                 expect(res.body.msg).to.equal("route not found");
               });
           });
-        });
-        describe("POST /api/articles:article_id/comments (no comment)", () => {
-          it("STATUS 400 bad input", () => {
+          it("STATUS 400 bad input (no comment)", () => {
             return request(app)
               .post("/api/articles/1/comments")
               .send()
@@ -192,9 +189,7 @@ describe("/", () => {
                 expect(res.body.msg).to.equal("bad request");
               });
           });
-        });
-        describe("POST /api/articles:article_id/comments (no username key present)", () => {
-          it("STATUS: 400 bad request", () => {
+          it("STATUS: 400 bad request (no username key present)", () => {
             return request(app)
               .post("/api/articles/1/comments")
               .send({ body: "down with this sort of thing!" })
@@ -203,9 +198,7 @@ describe("/", () => {
                 expect(res.body.msg).to.equal("bad request");
               });
           });
-        });
-        describe("POST /api/articles:article_id/comments (no body key present)", () => {
-          it("STATUS: 400 bad request", () => {
+          it("STATUS: 400 bad request (no body key present)", () => {
             return request(app)
               .post("/api/articles/1/comments")
               .send({ username: "lurker" })
@@ -214,9 +207,7 @@ describe("/", () => {
                 expect(res.body.msg).to.equal("bad request");
               });
           });
-        });
-        describe("POST /api/articles:article_id/comments (username does not match a user)", () => {
-          it("STATUS: 400 bad request", () => {
+          it("STATUS: 400 bad request (username does not match a user)", () => {
             return request(app)
               .post("/api/articles/1/comments")
               .send({ username: "bad username" })
@@ -225,9 +216,7 @@ describe("/", () => {
                 expect(res.body.msg).to.equal("bad request");
               });
           });
-        });
-        describe("POST /api/articles:article_id/comments (extra properties on request)", () => {
-          it("STATUS: 400 bad request", () => {
+          it("STATUS: 400 bad request (extra properties on request)", () => {
             return request(app)
               .post("/api/articles/1/comments")
               .send({
@@ -241,30 +230,18 @@ describe("/", () => {
               });
           });
         });
-        describe("GET /api/articles:article_id/comments (incorrect format article_id)", () => {
-          it("STATUS: 400 bad request", () => {
+        describe("GET", () => {
+          it("STATUS: 400 bad request (incorrect format article_id)", () => {
             return request(app)
-              .get("/api/articles/mitch/comments")
+              .get("/api/articles/four/comments")
               .expect(400)
               .then(res => {
                 expect(res.body.msg).to.equal("bad request");
               });
           });
-        });
-        describe("GET /api/articles:article_id/comments (no article at article_id)", () => {
-          it("STATUS: 404 route not found", () => {
+          it("STATUS: 404 route not found (no article at article_id)", () => {
             return request(app)
               .get("/api/articles/3000/comments")
-              .expect(404)
-              .then(res => {
-                expect(res.body.msg).to.equal("route not found");
-              });
-          });
-        });
-        describe("GET  /api/comment/comment_id_not_found", () => {
-          it("STATUS 404 route not found", () => {
-            return request(app)
-              .get("/api/articles/666")
               .expect(404)
               .then(res => {
                 expect(res.body.msg).to.equal("route not found");
@@ -278,8 +255,18 @@ describe("/", () => {
     ////////////////////////////////////////////
     describe("/api/comments/:comment_id", () => {
       //////////////////////////////////////////////
-      describe("PATCH  /api/comment/:comment_id (comment_id notfound)", () => {
-        it("STATUS 404 route not found", () => {
+      describe("GET", () => {
+        it("STATUS 404 route not found (invalid_id)", () => {
+          return request(app)
+            .get("/api/articles/666")
+            .expect(404)
+            .then(res => {
+              expect(res.body.msg).to.equal("route not found");
+            });
+        });
+      });
+      describe("PATCH ", () => {
+        it("STATUS 404 route not found (invalid_comment_id", () => {
           return request(app)
             .patch("/api/comments/4000")
             .send({ inc_votes: 3 })
@@ -288,9 +275,7 @@ describe("/", () => {
               expect(res.body.msg).to.equal("route not found");
             });
         });
-      });
-      describe("PATCH /comment/:comment_id wrong_format_id", () => {
-        it("STATUS:400 bad request", () => {
+        it("STATUS:400 bad request (wrong_format_id", () => {
           return request(app)
             .patch("/api/comments/six")
             .send({ inc_votes: 3 })
@@ -299,9 +284,7 @@ describe("/", () => {
               expect(res.body.msg).to.equal("bad request");
             });
         });
-      });
-      describe("PATCH /comment/:comment_id  (no data sent in patch request)", () => {
-        it("STATUS: 400 bad request", () => {
+        it("STATUS: 400 bad request (no data sent in patch request)", () => {
           return request(app)
             .patch("/api/comments/1")
             .send()
@@ -310,9 +293,7 @@ describe("/", () => {
               expect(res.body.msg).to.equal("bad request");
             });
         });
-      });
-      describe("PATCH /api/comment/:comment_id (no inc_votes key patch request)", () => {
-        it("STATUS: 400 bad request", () => {
+        it("STATUS: 400 bad request (no inc_votes key patch request)", () => {
           return request(app)
             .patch("/api/comments/1")
             .send({ randomKey: 2 })
@@ -321,9 +302,7 @@ describe("/", () => {
               expect(res.body.msg).to.equal("bad request");
             });
         });
-      });
-      describe("PATCH /api/comments/:comment_id (invalid inc_votes value)", () => {
-        it("STATUS: 400 bad request", () => {
+        it("STATUS: 400 bad request (invalid inc_votes value)", () => {
           return request(app)
             .patch("/api/comments/1")
             .send({ inc_votes: "yes" })
@@ -332,9 +311,7 @@ describe("/", () => {
               expect(res.body.msg).to.equal("bad request");
             });
         });
-      });
-      describe("PATCH /api/comments:comment_id (extra property on request body)", () => {
-        it("STATUS: 400 bad request", () => {
+        it("STATUS: 400 bad request (extra property on request body)", () => {
           return request(app)
             .patch("/api/comments/1")
             .send({ inc_votes: 2, mitch: "present" })
@@ -344,8 +321,8 @@ describe("/", () => {
             });
         });
       });
-      describe("DELETE /api/comments:comment_id (invalid comment_id format)", () => {
-        it("STATUS: 400 bad request", () => {
+      describe("DELETE", () => {
+        it("STATUS: 400 bad request (invalid comment_id format)", () => {
           return request(app)
             .delete("/api/comments/four")
             .expect(400)
@@ -353,8 +330,6 @@ describe("/", () => {
               expect(res.body.msg).to.equal("bad request");
             });
         });
-      });
-      describe("DELETE /api/comments:comment_id (no comment at comment_id)", () => {
         it("STATUS: 404 route not found", () => {
           return request(app)
             .delete("/api/comments/30000")
